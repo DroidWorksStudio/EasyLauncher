4e8df5ea94e5de4a1f301518e73d081fcbec9d09
HᴇCᴏᴅᴇs2ᴍᴜᴄʜ <wayne6324@gmail.com>
2024-05-21 08:25:28 +0100

    chore(release): prepare for version 0.0.6
    Signed-off-by: HᴇCᴏᴅᴇs2ᴍᴜᴄʜ <wayne6324@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "app.easy.launcher"
         minSdk = 24
         targetSdk = 34
-        versionCode = 5
-        versionName = "0.0.5"
+        versionCode = 6
+        versionName = "0.0.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
