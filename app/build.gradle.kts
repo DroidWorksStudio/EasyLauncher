fdc57fcea6ca3d383ba3a1842873383b81f3bb05
HeCodes2Much <wayne6324@gmail.com>
2024-05-22 16:50:35 +0100

    chore(release): prepare for version 0.0.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "app.easy.launcher"
         minSdk = 24
         targetSdk = 34
-        versionCode = 6
-        versionName = "0.0.6"
+        versionCode = 7
+        versionName = "0.0.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
