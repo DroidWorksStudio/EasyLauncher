3d6fbd3baf44aefc34ed607f7ccef66d94e15deb
HeCodes2Much <wayne6324@gmail.com>
2024-06-09 09:26:27 +0100

    Release: prepare for version 0.1.3
    Signed-off-by: HeCodes2Much <wayne6324@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "app.easy.launcher"
         minSdk = 24
         targetSdk = 34
-        versionCode = 12
-        versionName = "0.1.2"
+        versionCode = 13
+        versionName = "0.1.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         manifestPlaceholders["internetPermission"] = "android.permission.INTERNET"
