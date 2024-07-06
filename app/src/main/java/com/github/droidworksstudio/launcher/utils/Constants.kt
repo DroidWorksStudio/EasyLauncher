c98e008bf919e4e21b0c251c2bc1fbc384e13d76
HeCodes2Much <wayne6324@gmail.com>
2024-07-06 10:28:55 +0100

    Refactor: Changed Dotness font name.
    Signed-off-by: HeCodes2Much <wayne6324@gmail.com>

--- app/src/main/java/com/github/droidworksstudio/launcher/utils/Constants.kt
+++ app/src/main/java/com/github/droidworksstudio/launcher/utils/Constants.kt
@@ -166,7 +166,7 @@ object Constants {
     enum class Fonts {
         System,
         Bitter,
-        DotToDot,
+        Dotness,
         DroidSans,
         GreatVibes,
         Lato,
@@ -184,7 +184,7 @@ object Constants {
             return when (this) {
                 System -> Typeface.DEFAULT
                 Bitter -> ResourcesCompat.getFont(context, R.font.bitter)
-                DotToDot -> ResourcesCompat.getFont(context, R.font.dotness)
+                Dotness -> ResourcesCompat.getFont(context, R.font.dotness)
                 DroidSans -> ResourcesCompat.getFont(context, R.font.open_sans)
                 GreatVibes -> ResourcesCompat.getFont(context, R.font.great_vibes)
                 Lato -> ResourcesCompat.getFont(context, R.font.lato)
@@ -204,7 +204,7 @@ object Constants {
             return when (this) {
                 System -> context.getString(R.string.settings_font_system)
                 Bitter -> context.getString(R.string.settings_font_bitter)
-                DotToDot -> context.getString(R.string.settings_font_dotness)
+                Dotness -> context.getString(R.string.settings_font_dotness)
                 DroidSans -> context.getString(R.string.settings_font_droidsans)
                 GreatVibes -> context.getString(R.string.settings_font_greatvibes)
                 Lato -> context.getString(R.string.settings_font_lato)
