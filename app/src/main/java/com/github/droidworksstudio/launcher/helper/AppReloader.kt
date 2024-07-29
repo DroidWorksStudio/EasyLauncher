a9e55e2fbec764fedc5ab51abd85665d40ac5aa5
HeCodes2Much <wayne6324@gmail.com>
2024-07-29 17:14:31 +0100

    Refactor: Code Cleanup
    Signed-off-by: HeCodes2Much <wayne6324@gmail.com>

--- app/src/main/java/com/github/droidworksstudio/launcher/helper/AppReloader.kt
+++ app/src/main/java/com/github/droidworksstudio/launcher/helper/AppReloader.kt
@@ -11,6 +11,7 @@ object AppReloader {
         val intent = packageManager.getLaunchIntentForPackage(context.packageName)
         val componentName = intent?.component
         val mainIntent = Intent.makeRestartActivityTask(componentName)
+        mainIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_SINGLE_TOP)
 
         // Delay the restart slightly to ensure all current activities are finished
         Handler(Looper.getMainLooper()).post {
